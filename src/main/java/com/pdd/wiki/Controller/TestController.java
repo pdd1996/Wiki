9c460910308b21380561588194ae77691067f2ea
paddy <pdd337816464@163.com>
2023-03-04 00:40:58 +0800

    2-7 Http Client使用

--- src/main/java/com/pdd/wiki/Controller/TestController.java
+++ src/main/java/com/pdd/wiki/Controller/TestController.java
@@ -1,5 +1,6 @@
 package com.pdd.wiki.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello Post " + name;
     }
 }
