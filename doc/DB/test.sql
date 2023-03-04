2673cb968dffb33daf445b4c71fb14fd762a55f6
paddy <pdd337816464@163.com>
2023-03-04 17:12:36 +0800

    3-3 SpringBoot - mysql

---
+++ doc/DB/test.sql
@@ -0,0 +1,7 @@
+drop table if exists `test1`;
+
+create table `test` (
+    `id` bigint not null  comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment '测试';
