d4d0a4ec314d5a2a5fe4f4bdce94c3827edddb45
paddy <pdd337816464@163.com>
2023-03-04 15:14:20 +0800

    2-8 SpringBoot文件配置

--- src/main/java/com/pdd/wiki/Controller/TestController.java
+++ src/main/java/com/pdd/wiki/Controller/TestController.java
@@ -1,17 +1,21 @@
 package com.pdd.wiki.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:Test}")
+    private String testHello;
     /*
     常用 GET, POST, PUT, DELETE
      */
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World" + testHello;
     }
 
     @PostMapping("/hello/post")
