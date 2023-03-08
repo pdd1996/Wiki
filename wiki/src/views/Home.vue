ee21757943fa07cc232f41d66cfb2e8397cb9579
paddy <pdd337816464@163.com>
2023-03-08 22:16:39 +0800

    5-4 电子书列表显示

--- wiki/src/views/Home.vue
+++ wiki/src/views/Home.vue
@@ -1,89 +1,135 @@
 <template>
-    <a-layout-content style="padding: 0 50px">
-      <a-breadcrumb style="margin: 16px 0">
-        <a-breadcrumb-item>Home</a-breadcrumb-item>
-        <a-breadcrumb-item>List</a-breadcrumb-item>
-        <a-breadcrumb-item>App</a-breadcrumb-item>
-      </a-breadcrumb>
-      <a-layout style="padding: 24px 0; background: #fff">
-        <a-layout-sider width="200" style="background: #fff">
-           <a-menu
-            mode="inline"
-            v-model:selectedKeys="selectedKeys2"
-            v-model:openKeys="openKeys"
-            style="height: 100%"
-          >
-            <a-sub-menu key="sub1">
-              <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1
+  <a-layout-content style="padding: 0 50px">
+    <a-breadcrumb style="margin: 16px 0">
+      <a-breadcrumb-item>Home</a-breadcrumb-item>
+      <a-breadcrumb-item>List</a-breadcrumb-item>
+      <a-breadcrumb-item>App</a-breadcrumb-item>
+    </a-breadcrumb>
+    <a-layout style="padding: 24px 0; background: #fff">
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu mode="inline" v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" style="height: 100%">
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span>
+                <user-outlined />
+                subnav 1
+              </span>
+            </template>
+            <a-menu-item key="1">option1111</a-menu-item>
+            <a-menu-item key="2">option2</a-menu-item>
+            <a-menu-item key="3">option3</a-menu-item>
+            <a-menu-item key="4">option4</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <laptop-outlined />
+                subnav 2
+              </span>
+            </template>
+            <a-menu-item key="5">option5</a-menu-item>
+            <a-menu-item key="6">option6</a-menu-item>
+            <a-menu-item key="7">option7</a-menu-item>
+            <a-menu-item key="8">option8</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <notification-outlined />
+                subnav 3
+              </span>
+            </template>
+            <a-menu-item key="9">option9</a-menu-item>
+            <a-menu-item key="10">option10</a-menu-item>
+            <a-menu-item key="11">option11</a-menu-item>
+            <a-menu-item key="12">option12dsda</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+        <a-list 
+        item-layout="vertical" 
+        size="large" 
+        :pagination="pagination" 
+        :data-source="ebooks"
+        :grid="{ gutter: 25, column: 3 }"
+        >
+          <template #renderItem="{ item }">
+            <a-list-item key="item.bookName">
+              <template #actions>
+                <span v-for="{ type, text } in actions" :key="type">
+                  <component v-bind:is="type" style="margin-right: 8px" />
+                  {{ text }}
                 </span>
               </template>
-              <a-menu-item key="1">option1111</a-menu-item>
-              <a-menu-item key="2">option2</a-menu-item>
-              <a-menu-item key="3">option3</a-menu-item>
-              <a-menu-item key="4">option4</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-              <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-              </template>
-              <a-menu-item key="5">option5</a-menu-item>
-              <a-menu-item key="6">option6</a-menu-item>
-              <a-menu-item key="7">option7</a-menu-item>
-              <a-menu-item key="8">option8</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-              <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-              </template>
-              <a-menu-item key="9">option9</a-menu-item>
-              <a-menu-item key="10">option10</a-menu-item>
-              <a-menu-item key="11">option11</a-menu-item>
-              <a-menu-item key="12">option12dsda</a-menu-item>
-            </a-sub-menu>
-          </a-menu>
-        </a-layout-sider>
-        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-          <pre>{{ ebooks }}</pre>
-          <pre>{{ ebooks2 }}</pre>
-        </a-layout-content>
-      </a-layout>
-    </a-layout-content>
+              <a-list-item-meta :description="item.bookDesc">
+                <template #title>
+                  <a :href="item.href">{{ item.bookName }}</a>
+                </template>
+                <template #avatar><a-avatar :src="item.cover" /></template>
+              </a-list-item-meta>
+            </a-list-item>
+          </template>
+        </a-list>
+      </a-layout-content>
+    </a-layout>
+  </a-layout-content>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted, reactive, toRef } from 'vue';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-import axios from 'axios'
+import { UserOutlined, LaptopOutlined, NotificationOutlined, StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import axios from 'axios';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
   name: 'Home',
   components: {
     UserOutlined,
     LaptopOutlined,
     NotificationOutlined,
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
   },
   setup() {
     console.log("setup");
     const ebooks = ref();
-    const ebooks1 = reactive({books: []})
+    const ebooks1 = reactive({ books: [] })
 
     onMounted(() => {
       console.log("onMounted");
       axios.get("http://localhost:8882/ebook/list?bookName=Java").then((resp) => {
-      const data = resp.data;
-      ebooks.value = data.content;
-      ebooks1.books = data.content;
-      console.log(resp);
+        const data = resp.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(resp);
+      })
     });
-    })
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
 
     return {
       ebooks,
@@ -91,11 +137,12 @@ export default defineComponent({
       selectedKeys1: ref<string[]>(['2']),
       selectedKeys2: ref<string[]>(['1']),
       openKeys: ref<string[]>(['sub1']),
+      listData,
+      pagination,
+      actions,
     };
   },
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
