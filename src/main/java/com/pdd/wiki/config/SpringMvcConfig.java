210b0cbc34735f6f9d43763a37e148153d18077e
paddy <pdd337816464@163.com>
2023-03-10 22:00:30 +0800

    删除拦截器和过滤器

--- src/main/java/com/pdd/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/pdd/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package com.pdd.wiki.config;
-
-import com.pdd.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login");
-    }
-}
+//package com.pdd.wiki.config;
+//
+//import com.pdd.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/login");
+//    }
+//}
