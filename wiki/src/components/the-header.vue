cea79fbe422706c66f2b831026d31750a20f2d44
paddy <pdd337816464@163.com>
2023-03-10 22:31:26 +0800

    Revert "6-2 增加电子书管理页面"

--- wiki/src/components/the-header.vue
+++ wiki/src/components/the-header.vue
@@ -7,9 +7,14 @@
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
       >
-        <a-menu-item key="1">nav 1231</a-menu-item>
-        <a-menu-item key="2">nav 2214</a-menu-item>
-        <a-menu-item key="3">nav 34123</a-menu-item>
+        <a-menu-item key="home">
+          <RouterLink to="/">首页</RouterLink>
+        </a-menu-item>
+        <a-menu-item key="/admin/ebook">
+          <RouterLink to="/admin/ebook">电子书管理</RouterLink></a-menu-item>
+        <a-menu-item key="about">
+          <RouterLink to="/about">关于我们</RouterLink>
+        </a-menu-item>
       </a-menu>
     </a-layout-header>
 </template>
