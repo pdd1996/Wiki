b808abf083cf5f80601a453fbd1c461440893167
paddy <pdd337816464@163.com>
2023-06-01 23:09:55 +0800

    修改 - 完成3-6

--- src/main/java/com/pdd/wiki/controller/EbookController.java
+++ src/main/java/com/pdd/wiki/controller/EbookController.java
@@ -1,15 +1,14 @@
 package com.pdd.wiki.controller;
 
-import com.pdd.wiki.req.EbookReq;
+import com.pdd.wiki.domain.Ebook;
 import com.pdd.wiki.resp.CommonResp;
-import com.pdd.wiki.resp.EbookResp;
-import com.pdd.wiki.resp.PageResp;
 import com.pdd.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -18,13 +17,15 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
 
-
+    // 统一的返回
     @GetMapping("/list")
-    public CommonResp list(EbookReq req) {
-        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
-        PageResp<EbookResp> list = ebookService.list(req);
+    // CommonResp 统一返回 后端统一通用的返回格式
+    // 泛型 + List
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        // 数据库取来的数据赋值给 list
+        List<Ebook> list = ebookService.list();
         resp.setContent(list);
         return resp;
     }
-
 }
--- src/main/java/com/pdd/wiki/service/EbookService.java
+++ src/main/java/com/pdd/wiki/service/EbookService.java
@@ -1,62 +1,18 @@
 package com.pdd.wiki.service;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.pdd.wiki.domain.Ebook;
-import com.pdd.wiki.domain.EbookExample;
 import com.pdd.wiki.mapper.EbookMapper;
-import com.pdd.wiki.req.EbookReq;
-import com.pdd.wiki.resp.EbookResp;
-import com.pdd.wiki.resp.PageResp;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
-
 @Service
 public class EbookService {
-
-    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
 
-    public PageResp<EbookResp> list(EbookReq req) {
-
-        EbookExample ebookExample = new EbookExample();
-        EbookExample.Criteria criteria = ebookExample.createCriteria();
-
-        if (ObjectUtils.isEmpty(req.getBookName())){
-            criteria.andBookNameLike("%" + req.getBookName() + "%");
-        }
-
-        // 从1开始 与最近的查询关联
-        PageHelper.startPage(req.getPage(),req.getSize());
-        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-
-        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
-        // 总行数
-        pageInfo.getTotal();
-        LOG.info("总行数{}", pageInfo.getTotal());
-        // 总页数
-        pageInfo.getPages();
-        LOG.info("总页数{}", pageInfo.getPages());
-        List<EbookResp> respList = new ArrayList<>();
-
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-
-        PageResp<EbookResp> pageResp = new PageResp();
-        pageResp.setTotal(pageInfo.getTotal());
-        pageResp.setList(respList);
-        return pageResp;
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
     }
 }
