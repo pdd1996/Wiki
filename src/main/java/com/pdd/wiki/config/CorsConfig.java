bddc59172a2567b23205f21dcd49742283209645
Paddy_Pan <pdd337816464@outlook.com>
2023-06-06 19:46:47 +0800

    修改 - cors配置的注释

--- src/main/java/com/pdd/wiki/config/CorsConfig.java
+++ src/main/java/com/pdd/wiki/config/CorsConfig.java
@@ -12,8 +12,11 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
+                // 请求头
                 .allowedHeaders(CorsConfiguration.ALL)
+                // 请求方法
                 .allowedMethods(CorsConfiguration.ALL)
+                // 允许凭证 缓存
                 .allowCredentials(true)
                 .maxAge(3600); // 1小时内不需要再预检（发出OPTIONS请求）
     }
