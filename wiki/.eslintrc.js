a66a8cbc9a0f33f68cff55864f833b5aba49c291
Paddy_Pan <pdd337816464@outlook.com>
2023-06-06 21:18:57 +0800

    修改 eslint

--- wiki/.eslintrc.js
+++ wiki/.eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    // '@typescript-eslint/no-explicit-any': 0
   ],
   parserOptions: {
     ecmaVersion: 2020
