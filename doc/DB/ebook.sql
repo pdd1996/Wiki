f6dee7e16db398fa05b444e19e222923c8784869
Paddy_Pan <pdd337816464@outlook.com>
2023-06-24 18:20:35 +0800

    增加 - 新增ebook的sql文件

---
+++ doc/DB/ebook.sql
@@ -0,0 +1,5 @@
+INSERT INTO `ebook` (`id`, `book_name`, `category_1`, `category_2`, `book_desc`, `book_cover`, `doc_count`, `view_count`, `like_count`) VALUES (1, 'js', 1, 1, 'js必备123', '/image/JavaScript.jpg', 1, 1, 1);
+INSERT INTO `ebook` (`id`, `book_name`, `category_1`, `category_2`, `book_desc`, `book_cover`, `doc_count`, `view_count`, `like_count`) VALUES (2, 'Java', 2, 2, 'java必备', '/image/Typescript.jpg', 2, 2, 2);
+INSERT INTO `ebook` (`id`, `book_name`, `category_1`, `category_2`, `book_desc`, `book_cover`, `doc_count`, `view_count`, `like_count`) VALUES (3, 'css', 3, 3, 'css必备456', '/image/JavaScript.jpg', 3, 3, 3);
+INSERT INTO `ebook` (`id`, `book_name`, `category_1`, `category_2`, `book_desc`, `book_cover`, `doc_count`, `view_count`, `like_count`) VALUES (4, 'html', 4, 4, 'html必备', '/image/Typescript.jpg', 4, 4, 4);
+INSERT INTO `ebook` (`id`, `book_name`, `category_1`, `category_2`, `book_desc`, `book_cover`, `doc_count`, `view_count`, `like_count`) VALUES (5, 'c', 5, 5, 'c++必备 789', '/image/JavaScript.jpg', 5, 5, 5);
