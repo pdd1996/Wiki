1b2573293a207972e67d495cd9ca62731638694e
Paddy_Pan <pdd337816464@outlook.com>
2023-07-12 22:43:56 +0800

    增加 - 7-7 增加显示菜单

--- wiki/src/views/Home.vue
+++ wiki/src/views/Home.vue
@@ -7,42 +7,27 @@
     </a-breadcrumb>
     <a-layout style="padding: 24px 0; background: #fff">
       <a-layout-sider width="200" style="background: #fff">
-        <a-menu mode="inline" v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" style="height: 100%">
-          <a-sub-menu key="sub1">
+        <a-menu 
+          mode="inline" 
+          v-model:selectedKeys="selectedKeys2" 
+          v-model:openKeys="openKeys" 
+          style="height: 100%"
+        >
+          <a-menu-item key="welcome">
+            <router-link :to="'/'">
+              <span>欢迎</span>
+            </router-link>
+          </a-menu-item>
+          <a-sub-menu v-for="item in level1" :key="item.id" >
             <template #title>
               <span>
                 <user-outlined />
-                subnav 1
+                {{ item.name }}
               </span>
             </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
+            <a-menu-item v-for="child in item.children" :key="child.id">
+              <span>{{ child.name }}</span>  
+            </a-menu-item>
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
@@ -79,6 +64,8 @@
 import { defineComponent, ref, onMounted } from 'vue';
 import { UserOutlined, LaptopOutlined, NotificationOutlined, StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from 'axios';
+import { message } from 'ant-design-vue';
+import {Tool} from '../util/tool'
 
 const listData: Record<string, string>[] = [];
 
@@ -95,9 +82,11 @@ export default defineComponent({
   setup() {
     // 响应式的数据
     const ebooks = ref();
-
+    const level1 = ref();
+    let categorys: any
     // 初始化
     onMounted(() => {
+      handleQueryCategory();
       axios.get("/ebook/list",{
         params:{
           page: 1,
@@ -108,6 +97,24 @@ export default defineComponent({
         ebooks.value = data.content.list;
       })
     });
+
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组:",categorys);
+
+          level1.value = {};
+          level1.value = Tool.array2Tree(categorys,0)
+          console.log("树形结构：",level1.value)
+        } else {
+          message.error(data.message);
+        }
+      });
+    }; 
+
     const actions: Record<string, string>[] = [
       { type: 'StarOutlined', text: '156' },
       { type: 'LikeOutlined', text: '156' },
@@ -126,7 +133,9 @@ export default defineComponent({
           console.log(page)
         },
         pageSize: 3
-      }
+      },
+      level1,
+      handleQueryCategory
     };
   },
 });
